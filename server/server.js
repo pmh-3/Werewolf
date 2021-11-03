f7d104fb267cc4dfe66d84ade12f351a34b9c6dd
pmh-3 <71290233+pmh-3@users.noreply.github.com>
2021-11-02 19:19:23 -0700

    clean up

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
-//const { query } = require('express');
 const express = require('express'); 
-//const {readFileSync, writeFileSync} = require('fs');
-//const util = require('util');
+
 const http = require("http");
 const socketIo = require("socket.io");
 
@@ -10,10 +8,6 @@ const index = require("./routes/index");
 
 const app = express(); 
 app.use(index);
-
-//unused for sockets
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
 
@@ -45,21 +39,6 @@ const getApiAndEmit = socket => {
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
 
-//SOCKET IO
-// const io = require('socket.io')(app,{
-//     cors: {origin: "*"}
-// });
-
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-
-//     socket.on('message', (message) =>   {
-//         console.log(message);
-//         io.emit('message', `${socket.id.substr(0,2)} said ${message}` );    
-//     });
-// });
-
-
 
 const createRoom = () => {
     //init player array
@@ -84,34 +63,5 @@ const sendEaten = () => {
 const asignRole = () => {
 
 }
-
-
-// const serverFX = () => {
-//     app.get('/getPlays', (req, res) => {
-
-//         const count = readFileSync('./dummyDB.txt', 'utf-8');
-//         const newCount = parseInt(count) + 1;
-
-//         writeFileSync('./dummyDB.txt', String(newCount));
-//         console.log('new count: ', newCount);
-
-//         res.send({
-//             plays: String(newCount)
-//         })
-
-//     });
-
-//     app.post('/addLeader', (req, res) => {
-//         var Name = req.body.name;
-//         res.send(
-//         `I received your POST request. This is what you sent me: ${req.body.name}`,
-//         );
-//     });
-// }
-
-// app.listen(port, () => console.log(`Listening on port ${port}`)); 
-// serverFX();
-
-
 
 
