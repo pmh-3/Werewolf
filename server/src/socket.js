b1ee63b0f6959f7d3af5d31203edd1854f5fb847
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2021-11-16 15:28:59 -0800

    DELETE: function for all alive players

--- server/src/socket.js
+++ server/src/socket.js
@@ -28,7 +28,7 @@ const socket = (io) => {
   io.on("connection", (socket) => {
     console.log("A Player Connected");
     let s = socket.id;
-      /********************SET UP*************************** */
+    /********************SET UP*************************** */
     // When a player creates a room
     socket.on("createRoom", () => {
       // var clientIp = socket.request.connection.remoteAddress;
@@ -49,7 +49,7 @@ const socket = (io) => {
         let game = findGame(roomCode);
         console.log(`Game: ${game}`);
         // Create a new player and add it to the game
-        
+
         let player = new Player(playerName, socket.id);
         game.addPlayer(player);
         // Join the socket to the room requested
@@ -62,7 +62,6 @@ const socket = (io) => {
       }
     });
 
-
     // Get room
     socket.on("getRoom", (room) => {
       if (findGame(room) !== undefined) {
@@ -72,24 +71,24 @@ const socket = (io) => {
         socket.emit("roomNotFound");
       }
     });
-  /********************END SET UP*************************** */
+    /********************END SET UP*************************** */
 
-  // socket.on("update item", (arg1, arg2, callback) => {
-  //   console.log(arg1); // 1
-  //   console.log(arg2); // { name: "updated" }
-  //   callback({
-  //     status: "ok"
-  //   });
-  // });
+    // socket.on("update item", (arg1, arg2, callback) => {
+    //   console.log(arg1); // 1
+    //   console.log(arg2); // { name: "updated" }
+    //   callback({
+    //     status: "ok"
+    //   });
+    // });
 
-    //  // send role to each respective player using call back 
+    //  // send role to each respective player using call back
     // socket.on('getRole', (n, roomCode, callback) => {
     //   let game = findGame(roomCode);
     //   if(game == undefined){
     //     return;
     //   }
     //   console.log('assigning role ' + n);
-    
+
     //   game.players.forEach((p)=>{
     //     if(n == p.getPlayer().name ){
     //       //socket.emit("assignedRole", p.getPlayer().role);
@@ -98,95 +97,94 @@ const socket = (io) => {
     //     }
     //   })
     // });
-    
 
-
-  /********************STATE*************************** */
+    /********************STATE*************************** */
     // Start game
     socket.on("startGameRequest", (roomCode) => {
       if (findGame(roomCode) == undefined) {
-        socket.emit("roomNotFound", {error: "Room not found"});
-      }else{
+        socket.emit("roomNotFound", { error: "Room not found" });
+      } else {
         console.log("starting game");
         let game = findGame(roomCode);
-        switchState(game, 'intro');
+        switchState(game, "intro");
         clock(game);
       }
     });
 
-  
-  const switchState = (game, state)=>{
-    game.setState(state);
-    let roomCode = game.code;
-    switch(game.getState()){
-      case 'welcome':
-        io.to(roomCode).emit("goToNextPage", "welcomePage");
-        break;
+    const switchState = (game, state) => {
+      game.setState(state);
+      let roomCode = game.code;
+      switch (game.getState()) {
+        case "welcome":
+          io.to(roomCode).emit("goToNextPage", "welcomePage");
+          break;
 
+        case "intro":
+          // After client presses start game, all devices go to role page
+          game.assignPlayerRolesAndActions();
 
-      case 'intro':
-        // After client presses start game, all devices go to role page
-        game.assignPlayerRolesAndActions()
+          game.players.forEach((p) => {
+            let id = p.getId();
 
-        game.players.forEach((p)=>{
-          let id = p.getId();
-      
-          socket.broadcast.to(id).emit("assignedRole", p.getPlayer().role);
-            console.log('assigning ' + p.getPlayer().role + " to " + p.getPlayer().name + " " + p.getPlayer().socketId  + "orr" + p.getId())
-        })
+            socket.broadcast.to(id).emit("assignedRole", p.getPlayer().role);
+            console.log(
+              "assigning " +
+                p.getPlayer().role +
+                " to " +
+                p.getPlayer().name +
+                " " +
+                p.getPlayer().socketId +
+                "orr" +
+                p.getId()
+            );
+          });
 
+          io.to(roomCode).emit("goToNextPage", "rolePage");
+          io.to(roomCode).emit("startTimer", timer.intro);
+          console.log("intro");
+          break;
 
-        io.to(roomCode).emit("goToNextPage", "rolePage");
-        io.to(roomCode).emit("startTimer", timer.intro);
-        console.log("intro")
-        break;
+        case "night":
+          console.log("night");
+          io.to(roomCode).emit("goToNextPage", "nightPage");
+          io.to(roomCode).emit("startTimer", timer.night);
+          //Commence night votes
+          voting(game);
+          break;
 
+        case "sunrise":
+          console.log("sunrise");
+          let eaten = game.countVote(game.getState());
+          io.to(roomCode).emit("goToNextPage", "sunrisePage");
+          io.to(roomCode).emit("startTimer", timer.sunrise);
+          //send eaten to tv
+          break;
 
-      case 'night':
-        console.log("night");
-        io.to(roomCode).emit("goToNextPage", "nightPage");
-        io.to(roomCode).emit("startTimer", timer.night);
-        //Commence night votes
-        voting(game);
-        break;
+        case "day":
+          console.log("day");
+          io.to(roomCode).emit("goToNextPage", "dayPage");
+          io.to(roomCode).emit("startTimer", timer.day);
+          //commence day votes
+          voting(game);
+          break;
 
+        case "sunset":
+          let banished = game.countVote(game.getState());
+          console.log("sunset");
+          io.to(roomCode).emit("goToNextPage", "sunsetPage");
+          io.to(roomCode).emit("startTimer", timer.sunset);
+          //send banished to tv
+          break;
 
-      case 'sunrise':
-        console.log("sunrise");
-        let eaten = game.countVote(game.getState());
-        io.to(roomCode).emit("goToNextPage", "sunrisePage");
-        io.to(roomCode).emit("startTimer", timer.sunrise);
-        //send eaten to tv
-        break;
+        case "end":
+          console.log("end");
+          io.to(roomCode).emit("goToNextPage", "endPage");
+          io.to(roomCode).emit("startTimer", timer.end);
+          break;
+      }
+    };
 
-
-      case 'day':
-        console.log("day");
-        io.to(roomCode).emit("goToNextPage", "dayPage");
-        io.to(roomCode).emit("startTimer", timer.day);
-        //commence day votes
-        voting(game);
-        break;
-
-
-      case 'sunset':
-        let banished = game.countVote(game.getState());
-        console.log("sunset");
-        io.to(roomCode).emit("goToNextPage", "sunsetPage");
-        io.to(roomCode).emit("startTimer", timer.sunset);
-        //send banished to tv
-        break;
-
-
-      case 'end':
-        console.log("end")
-        io.to(roomCode).emit("goToNextPage", "endPage");
-        io.to(roomCode).emit("startTimer", timer.end);
-        break;
-     }
-  }
-   
-    const clock = (game) =>{
+    const clock = (game) => {
       const rolePageTime = timer.intro;
       const nightPageTime = timer.night;
       const sunrisePageTime = timer.sunrise;
@@ -215,87 +213,66 @@ const socket = (io) => {
             sunsetPageTime +
             endPageTime
         ) {
-          switchState(game,'night');
+          switchState(game, "night");
         } else if (
           totalGameTime ===
           sunrisePageTime + dayPageTime + sunsetPageTime + endPageTime
         ) {
-          switchState(game,'sunrise');
+          switchState(game, "sunrise");
         } else if (
           totalGameTime ===
           dayPageTime + sunsetPageTime + endPageTime
         ) {
-          switchState(game,'day');
+          switchState(game, "day");
         } else if (totalGameTime === sunsetPageTime + endPageTime) {
-          switchState(game,'sunset');
+          switchState(game, "sunset");
         } else if (totalGameTime === endPageTime) {
-          switchState(game,'end');
+          switchState(game, "end");
         } else if (totalGameTime === 0) {
-          switchState(game,'welcome');
+          switchState(game, "welcome");
           clearInterval(countDown);
         }
       }, 1000);
-    }
+    };
     /********************END STATE*************************** */
 
     /********************VOTING*************************** */
 
-    const voting = (game)=>{
+    const voting = (game) => {
       console.log("voting");
       let roomCode = game.code;
 
-      if(game.state == 'night'){
-        console.log('night vote')
+      if (game.state == "night") {
+        console.log("night vote");
         io.to(roomCode).emit("startVoting", players, wolves, villagers);
-      }else{
+      } else {
         io.to(roomCode).emit("startVoting", players, undefined, undefined);
       }
 
       // Zi: process temporary vote (from wolf during night and from everyone during day)
       socket.on("sendTemporaryVote", (roomCode, playerName, playerTarget) => {
         io.to(roomCode).emit("temporaryVote", playerName, playerTarget);
-      }) 
+      });
 
       socket.on("submitVote", (voterRole, targetedPlayer) => {
-        // If voter was seer, send back targeted player's identity 
-        if (voterRole === "TESTSEER") 
+        // If voter was seer, send back targeted player's identity
+        if (voterRole === "TESTSEER")
           io.to(roomCode).emit("revealIdentity", "SUPERSTAR");
-        
-        if (voterRole === "wolf"){
-          game.Vote(targetedPlayer,'add');
+
+        if (voterRole === "wolf") {
+          game.Vote(targetedPlayer, "add");
         }
-        if(voterRole === 'healer'){
-          game.Vote(targetedPlayer,'delete');
+        if (voterRole === "healer") {
+          game.Vote(targetedPlayer, "delete");
         }
-      }); 
-    }
-       /******************** END VOTING*************************** */
-
-
-    // Jason: send a list of alive player upon request
-    socket.on("req-tv-allplayers-fullinfo", roomCode => {
-
-      // Game Object should have a list of player
-      // I am constructing a dumy player object list
-      const playerObjList = [];
-      let i = 0;
-      for (const playerName of players) {
-        // param: player's name& socket ID
-        let playerObj = new Player(playerName, i);
-        playerObj.assignRoleAndAction();
-        console.log(playerObj)
-        playerObjList.push(playerObj);
-        i++;
-      }
-      io.to(roomCode).emit("res-tv-allplayers-fullinfo", playerObjList);
-    })
-
+      });
+    };
+    /******************** END VOTING*************************** */
 
     socket.on("disconnect", () => {
       // TODO: Remove player from game
       console.log("user disconnected");
     });
-
   });
 };
 
