7deef369ad9892f55303a6c6a014340e7ce6486f
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2021-11-18 00:43:45 -0800

    UPDATE: Client SOCKET_URL to http://localhost:5000

--- client/src/components/services/Socket.js
+++ client/src/components/services/Socket.js
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-const SOCKET_URL = "http://localhost:5005";
+const SOCKET_URL = "http://localhost:5000";
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = React.createContext();
