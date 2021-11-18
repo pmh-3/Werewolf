526e7b6b1e90d0f02f736cbe77318168859035f1
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2021-11-18 08:18:01 -0800

    ADD: Cors

--- server/src/index.js
+++ server/src/index.js
@@ -6,14 +6,12 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http, { cors: { origin: "*" } });
 const socket = require("./socket")(io);
 const port = process.env.PORT || 8080;
+const cors = require("cors");
 
 const helmet = require("helmet");
 
 app.use(helmet());
-app.use(express.static(path.join(__dirname, "../../client/build")));
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
-});
 
 http.listen(port, () => console.log(`Listening on port ${port}`));
