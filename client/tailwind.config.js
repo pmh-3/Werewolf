5d2d133a8497caaa3d3013ed18bf8f1a5b9d7451
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2021-11-18 01:12:20 -0800

    ADD: Purge CSS rule to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,38 +1,38 @@
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'spooky': ['carnivalee-freakshow'],
-      'read': ['ui-monospace'],
+      spooky: ["carnivalee-freakshow"],
+      read: ["ui-monospace"],
     },
     colors: {
-      'orange': '#d87d05',
-      'teeth': '#FFFFFF',
-      'darkOrange': '#b06604',
-      'ice': '#074355',
+      orange: "#d87d05",
+      teeth: "#FFFFFF",
+      darkOrange: "#b06604",
+      ice: "#074355",
     },
     fontSize: {
-      'computer': '10rem',
-      'device': '7rem',
-      'TV': '15rem',
-      'header': '6rem',
-      'large': '3rem',
-      'medium': '2rem',
-      'small': '1rem',
-      'button': '2rem',
+      computer: "10rem",
+      device: "7rem",
+      TV: "15rem",
+      header: "6rem",
+      large: "3rem",
+      medium: "2rem",
+      small: "1rem",
+      button: "2rem",
     },
     screens: {
-    'small': '640px',
+      small: "640px",
     },
     extend: {
       inset: {
-        '1/5':'20%'
-      }
+        "1/5": "20%",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
