a3d0d6e9e603a3f961284d0c6b63611b14f0ce27
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2021-11-18 08:51:10 -0800

    UPDATE: SOCKET_URL to match server

--- client/src/components/services/Socket.js
+++ client/src/components/services/Socket.js
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-const SOCKET_URL = "http://localhost:5005";
+const SOCKET_URL = "http://http://45.79.65.55/";
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = React.createContext();
