658f2f91434f2c972675092ef6bd8aef2642ae2f
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2021-11-18 08:45:06 -0800

    ADD: Cors and Update PORT

--- server/src/index.js
+++ server/src/index.js
@@ -4,14 +4,16 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, { cors: { origin: "*" } });
 const socket = require("./socket")(io);
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 8080;
+const cors = require("cors");
 
 const helmet = require("helmet");
 
 app.use(helmet());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Server is running");
 });
 
 http.listen(port, () => console.log(`Listening on port ${port}`));
