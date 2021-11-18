bbb1dd8bbfb2347b45c1561fc3113c03694a096b
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2021-11-18 09:18:30 -0800

    UPDATE: SOCKET_URL to match prod server

--- client/src/components/services/Socket.js
+++ client/src/components/services/Socket.js
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-const SOCKET_URL = "http://http://45.79.65.55/";
+const SOCKET_URL = "https://react-werewolf-server.herokuapp.com/";
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = React.createContext();
